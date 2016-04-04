5f862c7e5641361465e4167a9d5196ee8fd4d5f8
Simon Eddeland <eddeland@gmail.com>
2016-04-04 22:20:13 +0200

    Updated CNavigation, added support for submenus

--- src/CNavigation/CNavigation.php
+++ src/CNavigation/CNavigation.php
@@ -1,10 +1,34 @@
 <?php
 class CNavigation {
+    /**
+     * Generates an HTML menu (with support for 1 submenulevel) from the specified array
+     * @param none $items An array with information about the menu
+     */
   public static function GenerateMenu($items) {
     $html = "<nav class='{$items['class']}'>\n";
     foreach($items['items'] as $key => $item) {
-      $selected = basename($_SERVER['SCRIPT_FILENAME']) == $item['url'] ? 'selected' : null;
-      $html .= "<a href='{$item['url']}' class='{$selected}'>{$item['text']}</a>\n";
+      // Check if item is a menu item or a submenu
+      if (isset($item['submenuname'])) {
+
+          // Generate the menu for the submenuitems
+          $submenuHtml = "";
+          $submenuClass ="submenu";
+          foreach ($item['submenuitems'] as $key => $submenuitem) {
+             $selected = basename($_SERVER['SCRIPT_FILENAME']) == $submenuitem['url'] ? 'selected' : null;
+             $submenuClass = is_null($selected) ? $submenuClass : $submenuClass . ' selected';
+             $submenuHtml .= "<a href='{$submenuitem['url']}' class='{$selected}'>{$submenuitem['text']}</a>\n";
+          }
+
+          // Generate the HTML of the enclosing div for the submenu
+          $parentHtml = "<div class='{$submenuClass}'>{$item['submenuname']}<div class='submenulist'>";
+          $submenuCloseHtml = "</li></div></div>\n";
+
+          // Put it all together
+          $html .= $parentHtml . $submenuHtml . $submenuCloseHtml;
+      } else {
+          $selected = basename($_SERVER['SCRIPT_FILENAME']) == $item['url'] ? 'selected' : null;
+          $html .= "<a href='{$item['url']}' class='{$selected}'>{$item['text']}</a>\n";
+      }
     }
     $html .= "</nav>\n";
     return $html;
